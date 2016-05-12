df2e4119d35e82f25d0bdb30691601a4eb13519a
pantol <pantolik@rambler.ru>
2016-05-12 17:35:24 +0300

    Сделал расчет сметы по квартире (код не причесан)

--- flat.py
+++ flat.py
@@ -79,4 +79,103 @@ math.floor(4.2) # 4
 Итого: 25000 руб.
 
 """
+import math
+class Room ():
+    def __init__(self,l,w,h,win = 1.2,door = 1):
+        self.l = l
+        self.h = h
+        self.w = w
+        self.win = win
+        self.door = door
+    def floor(self,lam):
+        pl = self.l*self.w
+        q = pl/lam.ss()
+        return (q)
+    def param(self):
+        return ([self.l,self.w,self.h])
+    def walls (self,wp):
+        per = (self.l+self.w)*2-self.win - self.door
+        pl = per * self.h
+        return (pl/wp.ss())
+    def ceil(self,pn):
+        return (self.l*self.w/pn.ss())
+    def countr(self, wp,pn,lam):
+        oboi = (wp.pr(),math.ceil(self.walls(wp)),math.ceil(self.walls(wp))*wp.pr())
+        kraska = (pn.pr(),math.ceil(self.ceil(pn)),math.ceil(self.ceil(pn))*pn.pr())
+        laminat = (lam.pr(),math.ceil(self.floor(lam)),math.ceil(self.floor(lam))*lam.pr())
+        #print (l1)
+        return (oboi,kraska,laminat)
+    
+class Material():
+    def __init__(self,s,price):
+        self.s = s
+        self.price = price
+class Wallpaper(Material):
+    def __init__(self,s, price, l, w):
+        super().__init__(s,price)
+        self.l = l
+        self.w = w
+    def pr(self):
+        return (self.price)
+    def ss(self):
+        return (self.l*self.w)# сколько один рулон покрывает стен
+class Laminat(Material):
+    def __init__(self,s, price, l, w,n):
+        super().__init__(s,price)
+        self.l = l
+        self.w = w
+        self.n = n
+    def ss(self):
+        return (self.l*self.w*self.n)# сколько одна упаковка покрывает пола
+    def pr(self):
+        return (self.price)
+class Paint(Material):
+    def __init__(self,s, price, weight, cons):
+        super().__init__(s,price)
+        self.weight = weight # в кг
+        self.cons = cons #кг на кв.м
+    def ss(self):
+        return (self.weight/self.cons) #сколько одной баанкой можно покрасить кв.м.
+    def pr(self):
+        return (self.price)
+class Flat():
+    def __init__(self):
+        self.myflat=[]
+    def add(self,room):
+        self.myflat.append(room)
+    def rem(self,room):
+        self.myflat.remove(room)
+        
+    def countf(self):
+        smeta=0
+        for i in self.myflat:
+            for j in range(3):
+                smeta += i.countr(wp,pp,lam1)[j][2]
+        return (smeta)
+    def show(self):
+        return (self.myflat)
+        
+wp = Wallpaper(1,2000,10,0.5)
+lam1=Laminat(1,1200,1.2,0.2,6)
+pp = Paint(1,5000,5,0.25)
+myroom = Room (4, 5, 2.5, 1.5,1)
+mr = Room (5, 3, 2.5, 1.5,1)
+mr1 = Room (4.1, 3.5, 2.5, 1,0.9)
 
+#g = myroom.countr(wp,pp,lam1)
+
+f = Flat()
+f.add(myroom)
+f.add(mr)
+f.add(mr1)
+#print (f.countf())
+print ('Смета на квартиру:')
+for i in f.show():
+    print ("-"*30)
+    print ('[Комната: длина = {} м, ширина = {} м, высота ={} м]'.format(i.param()[0],i.param()[1],i.param()[2]))
+    rr = i.countr(wp,pp,lam1)
+    print ('Обои     {:5} * {:2} = {:6} руб'.format(rr[0][0],rr[0][1],rr[0][2]))
+    print ('Краска   {:5} * {:2} = {:6} руб'.format(rr[1][0],rr[1][1],rr[1][2]))
+    print ('Ламинат  {:5} * {:2} = {:6} руб'.format(rr[2][0],rr[2][1],rr[2][2]))
+print ("="*30)
+print ('ИТОГО:   {} руб.'.format(f.countf()))
